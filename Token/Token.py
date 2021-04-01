3addad780518633c76227daad950fe21d2f5c2f0
Gustavo Augusto <gacl@ic.ufal.br>
2021-04-01 14:22:29 -0300

    Adicionado parametros de categoria do token

--- Token/Token.py
+++ Token/Token.py
@@ -1,8 +1,8 @@
 class Token():
 
-  def __init__(self, line_number, column_number, lexeme):
+  def __init__(self, line_number, column_number, lexeme, token_category):
     self.line_number = line_number
     self.column_number = column_number
     self.lexeme = lexeme
-    self.token_category
-    self.token_id
+    self.token_category = token_category
+    # self.token_id
