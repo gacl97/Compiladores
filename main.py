a6406a81115a49994365475fc02c772641b6c118
Gustavo Augusto <gacl@ic.ufal.br>
2021-04-02 19:32:47 -0300

    Adicionado leitura por argumentos

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from Syntatic.Syntatic import Syntatic
-
+import sys
 
 def main():
-  Syntatic()
+  if(len(sys.argv) <= 1):
+    print("No path directory was passed in arguments.")
+    return
+
+  for i in range(1, len(sys.argv)):
+    file_name = sys.argv[i]
+    Syntatic(file_name)
+    print("\n\n")
+
 main()
